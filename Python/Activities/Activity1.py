71983a9dba79dc22e83d6dee445c3ba20c7f3917
subham-lab <77741348+subham-lab@users.noreply.github.com>
2023-06-06 12:07:43 +0530

    Add files via upload

---
+++ Python/Activities/Activity1.py
@@ -0,0 +1,6 @@
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+year = 2023 - age + 100
+print(year)
+print(str(name)+ " will be 100 years old in the year" + str(year))
+print(f"{name} will be 100 years old in the year {year}")
