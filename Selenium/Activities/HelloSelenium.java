a8b6b1b90cf25cd9f5aa62fbb4ecb525016b62ac
subham-lab <77741348+subham-lab@users.noreply.github.com>
2023-06-13 13:21:21 +0530

    FST_Selenium

---
+++ Selenium/Activities/HelloSelenium.java
@@ -0,0 +1,23 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class HelloSelenium {
+        public static void main(String[] args) {
+            // Setup the Firefox driver(GeckoDriver)
+            WebDriverManager.firefoxdriver().setup();
+
+            // Create a new instance of the Firefox driver
+            WebDriver driver = new FirefoxDriver();
+
+            // Open the browser
+            driver.get("https://training-support.net");
+
+            // Perform testing and assertions
+
+            // Close the browser
+            // Feel free to comment out the line below
+            // so it doesn't close too quickly
+            driver.quit();
+    }
+}
